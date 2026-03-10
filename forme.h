b00dfb112e571f3c7a4f1e449a430c271e9a1be5
Gabriel Simon Sabin Plewinski <plewinsk@ic1-ubt22-5049.intranet.epfl.ch>
2026-03-10 17:03:23 +0100

    crea getter forme

--- forme.h
+++ forme.h
@@ -4,6 +4,8 @@ class Point {
     Point (double x, double y)
     :x(x),y(y)
 
+    double get_x() const { return x; }
+    double get_y() const { return y; }  
     protected:
 
     double x;
@@ -12,18 +14,24 @@ class Point {
 
 class Cercle: public Point {
 
+    public:
     Cercle (Point centre,double rayon)
     :centre(centre),rayon(rayon)
     {}
+    double get_rayon() const{return rayon;}
 
+    private:
     double rayon;
 };
 
 struct Carre : public Point {
 
-    Cercle (Point centre,double taille)
+    public:
+    Carre (Point centre,double taille)
     :centre(centre),taille(taille)
     {}
-
+    double get_taille() const{return taille;}
+    
+    private:
     double taille;
 };
