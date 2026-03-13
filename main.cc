f45f52f4ca4051e4f188dae8874aef0eaddb3892
Jean-Michel Vellozzi <Jmvellozzi@gmail.com>
2026-03-13 12:38:28 +0100

    squellete main argc argv

--- main.cc
+++ main.cc
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <cstdlib>
+#include <fstream>
 using namespace std;
 
-int main() {
-    cout << "rpz 01630" << endl;
+
+
+int main(int argc, *char argv) {
+    
+    ifstream fichier_test(argv[1]);
+    if (fichier_test.fail()){
+        return EXIT_FAILURE;
+    }
+//appel de la fonction de lecture avec (argv[1])aka "test.0x"
+
+
+
+
+
     return 0;
 }
