08904a4e0a608c3214e0447f69af3241b074b746
Gabriel Simon Sabin Plewinski <plewinsk@ic1-ubt22-5044.intranet.epfl.ch>
2026-03-13 12:03:41 +0100

    crea func dist+norme

--- tools.h
+++ tools.h
@@ -1,41 +1,38 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
-class Point {
-    public:
+#include "message.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Point {
+  
 
     Point (double x, double y)
     :x(x),y(y)
+    {}
 
-    double get_x() const { return x; }
-    double get_y() const { return y; } 
-
-    protected:
     double x;
     double y;
 };
 
-class Cercle: public Point {
+struct Cercle : Point{
 
-    public:
     Cercle (Point centre,double rayon)
     :centre(centre),rayon(rayon)
     {}
-    double get_rayon() const{return rayon;}
 
-    private:
     double rayon;
 };
 
-struct Carre : public Point {
+struct Carre : Point {
 
-    public:
+
     Carre (Point centre,double size)
     :centre(centre),size(size)
     {}
-    double get_taille() const{return size;}
-    
-    private:
+
     double size;
 };
 
@@ -43,8 +40,9 @@ constexpr double epsil_zero = 0.125
 
 namespace Tools {
     bool test_range(double size,double lim_min,double lim_max,bool with_epsil);
-
-
+    void error_message(string message);
+    double norme(double x, double y);
+    double distance(Point p1, Point p2);
 }
 
 #endif // TOOLS_H
