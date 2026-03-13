01a25f9e88b744e88f958a60314a4afadbb5e11e
Gabriel Simon Sabin Plewinski <plewinsk@ic1-ubt22-5051.intranet.epfl.ch>
2026-03-13 09:24:01 +0100

    crea test range

--- brick.cc
+++ brick.cc
@@ -1,7 +1,9 @@
 #include "brick.h"
+#include "message.h"
+#include "tools.h"
+#include "constants"
 
 void Brick :: test(){
-
-
+    if (Tools::test_range(brick.size,brick_size_min,arena_size))
     
 }
