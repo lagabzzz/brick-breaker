7977c0dfae88a35b3a3b79ebf97cbffe95ceb1c4
Gabriel Simon Sabin Plewinski <plewinsk@ic1-ubt22-5021.intranet.epfl.ch>
2026-04-16 15:48:42 +0200

    depl paddle okok

--- gui.cc
+++ gui.cc
@@ -333,7 +333,7 @@ void My_window::on_drawing_move(double x, double y)
 {
     if(!loop_activated) return; 
     double norm_x = ((x-(m_width-m_side)/2) / m_side) * arena_size;
-    double norm_y = (y / m_side) * arena_size + (m_height-m_side)/2;
+    double norm_y = (y / m_side) * arena_size;
     if (norm_y >= 100.0 - 1.5*game.get_paddle_radius()){
         
         game.set_paddle_x(norm_x);
