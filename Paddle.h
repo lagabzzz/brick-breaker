95eae71585366b9ffe079bcb7a095635d321d466
Jean-Michel Vellozzi <Jmvellozzi@gmail.com>
2026-04-17 15:19:09 +0200

    collision briques a peu pres ok

--- Paddle.h
+++ Paddle.h
@@ -18,7 +18,9 @@ public:
     double get_y() const { return paddle.centre.y; }
     double get_rayon() const { return paddle.rayon; }
     void set_follow_mouse(double x);
+    double get_follow_mouse() const {return follow_mouse; }
     void set_paddle_x();
+    void set_paddle_x(double futur_x) {paddle.centre.x = futur_x;}
     const Circle& get_paddle() const;
     bool inclusion_arene();
     void draw_paddle();
