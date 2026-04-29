7ee3440030728457e6d289716f2055e353d6b912
Jean-Michel Vellozzi <Jmvellozzi@gmail.com>
2026-04-29 16:57:24 +0200

    paddle et button

--- paddle.cc
+++ paddle.cc
@@ -6,14 +6,15 @@ const Circle& Paddle::get_paddle() const{
 }
 
 bool Paddle::inclusion_arene(){
-
     double centre_y = paddle.centre.y;
-    double offset = sqrt(paddle.rayon * paddle.rayon - centre_y * centre_y);
+    double offset = paddle.rayon * paddle.rayon - centre_y * centre_y;
+    if (offset < 0) return true;
+    offset = sqrt(offset);
     double min_x = offset;
     double max_x = arena_size - offset;
     bool testx (Tools::test_range(paddle.centre.x,min_x, max_x,false));
     
-    bool testy (Tools::test_range(paddle.centre.y,-paddle.rayon,0,false));
+    bool testy ((centre_y + paddle.rayon) > 0.0);
 
       if((testx and testy)==false){
 
